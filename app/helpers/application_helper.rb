5803a4cb8d9d45480d565cbb4f694353ca9f5e3b
johnsonsirv <johnsonsirv@gmail.com>
2019-10-22 18:00:56 +0000

    update UI for find friends

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	 def gravatar_for(user, size: 120)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.fullname)
+  end
 end
