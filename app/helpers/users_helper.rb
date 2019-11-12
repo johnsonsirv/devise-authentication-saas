18c0ef9345dff9ecc31f8132c358465f8f83fb80
johnsonsirv <johnsonsirv@gmail.com>
2019-11-12 23:59:08 +0000

    updated users#index view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,63 +1,63 @@
 module UsersHelper
 	
-	def show_banner_call_to_action_for(user)
+	def show_banner_call_to_action_for(user, btn1='btn-timeline-friend', btn2='btn-timeline-friend')
 		return update_info_btn if current_user == user
     
-    return pending_friendship_btn(user) if current_user
+    return pending_friendship_btn(user, btn2) if current_user
     .has_pending_friend_request_to?(user)
 		
-    return accept_friend_request_btns(user) if current_user
+    return accept_friend_request_btns(user, btn1, btn2) if current_user
     .has_pending_friend_request_from?(user)
     
-    return unfriend_btn(user) if current_user.friends_with?(user)
+    return unfriend_btn(user, btn2) if current_user.friends_with?(user)
 		
-    friendship_btn(user)
+    friendship_btn(user, btn1)
 	end
 	
 	def update_info_btn
 		link_to 'Update Info', '#', class: "btn btn-timeline-friend", title: "Update Info"
 	end
 	
-	def friendship_btn(user)
+	def friendship_btn(user, btn1)
 		link_to 'Add Friend', friendships_path(other_user: user),  
     method: :post, 
-    class: "btn btn-timeline-friend", 
-    title: "Send friend Request"
+    class: "btn #{btn1}", 
+    title: 'Send friend Request'
 	end
   
-  def pending_friendship_btn(user)
+  def pending_friendship_btn(user, btn2)
     link_to 'Friend Request sent', friendship_path(user),  
     method: :delete, 
     data: { confirm: 'Are you sure you want to remove this request?' },
-    class: "btn btn-timeline-friend", 
+    class: "btn #{btn2}", 
     title: "Delete friend request"
   end
   
-  def accept_friend_request_btns(user)
-    "#{confirm_friendship_btn(user)}
-    #{delete_friendship_btn(user)}".html_safe
+  def accept_friend_request_btns(user, btn1, btn2)
+    "#{confirm_friendship_btn(user, btn1)}
+    #{delete_friendship_btn(user, btn2)}".html_safe
   end
   
-  def confirm_friendship_btn(user)
+  def confirm_friendship_btn(user, btn1)
     link_to 'Confirm Friend', friendship_path(user),  
     method: :patch, 
-    class: "btn btn-timeline-friend", 
+    class: "btn #{btn1}", 
     title: "Confirm friend request"
   end
   
-   def delete_friendship_btn(user)
+   def delete_friendship_btn(user, btn2)
     link_to 'Remove Request', friendship_path(user),  
     method: :delete, 
     data: { confirm: 'Are you sure you want to remove this request?' },
-    class: "btn btn-timeline-friend", 
+    class: "btn #{btn2}", 
     title: "Delete friend request"
   end
   
-  def unfriend_btn(user)
+  def unfriend_btn(user, btn2)
     link_to 'Friends', friendship_path(user),  
     method: :delete, 
     data: { confirm: 'You are about to unfriend this friend?' },
-    class: "btn btn-timeline-friend", 
+    class: "btn #{btn2}", 
     title: "Click to Unfriend"
   end
   
