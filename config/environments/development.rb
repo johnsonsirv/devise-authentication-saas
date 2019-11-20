61b7c94d3c594fa8e4095db3059c439cd146b3dd
johnsonsirv <johnsonsirv@gmail.com>
2019-11-20 11:34:18 +0000

    whiteliste useed eager loading

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,7 @@ Rails.application.configure do
     # Bullet.growl         = true
       Bullet.rails_logger  = true
       Bullet.add_footer    = true
+      Bullet.add_whitelist :type => :unused_eager_loading, :class_name => "Friendship", :association => :user
     end  # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
